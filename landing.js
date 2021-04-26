9c48fc07ae7ac41d2aaa0861c206471ca5d297f0
Wisha Wa <wisha.wa@yandex.com>
2021-04-26 16:24:12 +0000

    make id decimal only

--- landing.js
+++ landing.js
@@ -4,14 +4,14 @@
 const domRoomIdInput = document.getElementById('room-id-input');
 const domNameInput = document.getElementById('name-input');
 
-function getRandomString() {
-    return Math.random().toString(36).substr(2, 8);
+function getRandomIdString() {
+    return Math.random().toString().substr(2, 8);
 }
 
 function createRoom() {
     const playerName = domNameInput.value;
-    const sessionId = getRandomString();
-    const playerId = getRandomString();
+    const sessionId = getRandomIdString();
+    const playerId = getRandomIdString();
     const sessionDocRef = getSessionRef(sessionId);
     
     sessionDocRef.set({
@@ -36,7 +36,7 @@ function createRoom() {
 function joinRoom() {
     const sessionId = domRoomIdInput.value;
     const playerName = domNameInput.value;
-    const playerId = getRandomString();
+    const playerId = getRandomIdString();
     const sessionDocRef = getSessionRef(sessionId);
     sessionDocRef.child('players').child(playerId).set({
         name: playerName,
