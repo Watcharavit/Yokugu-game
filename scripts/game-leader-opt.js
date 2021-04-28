5826960bb66d34bb9ae0059b80f6ef39cfaf7ee9
Pemtut Soisangtong <pemzar007@hotmail.com>
2021-04-28 20:27:51 +0700

    Add game-leader-opt.js

---
+++ scripts/game-leader-opt.js
@@ -0,0 +1,7 @@
+document.getElementById('btn-pick-categories').onclick = function(){
+    window.location = "game-leader-opt-categories.html"
+}
+
+document.getElementById('btn-custom-words').onclick = function() {
+    window.location = "game-leader-opt-custom.html"
+}
